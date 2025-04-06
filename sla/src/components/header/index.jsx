a02305845ef88694e0eef6aa1f398844982592f1
lucas.dpsa01 <lucasdsa2801@gmail.com>
2025-04-06 01:04:38 -0300

    uptade

---
+++ sla/src/components/header/index.jsx
@@ -0,0 +1,15 @@
+import "./header.css"
+
+export default function Header(){
+    return (
+        <div className="header">
+            <h1>Portifolio</h1>
+            <div className="header-links">
+                <h3>Contato</h3>
+                <h3>Skills</h3>
+                <h3>Sobre</h3>
+                <h3>Projetos</h3>
+            </div>
+        </div>
+    )
+}
---
+++ sla/src/components/introdução/index.jsx
@@ -0,0 +1,15 @@
+import minhafoto from "../../assets/minha_foto.jpg"
+
+import "./introducao.css"
+
+export default function Introducao(){
+    return(
+        <div className="introducao">
+            <div className="introducao-text">
+                <h1>Olá, eu sou Paiva Sá</h1>
+                <p>Estou sempre aprimorado minhas skills em React and Node para ser um programador full-stack. Meu objetivo é criar ferramentas que ajudem as pessoas e sempre buscando conhecimento.</p>
+            </div>
+            <img src={minhafoto} alt="minha foto" height={250} className="introducao-imagem"/>
+        </div>
+    )
+}
