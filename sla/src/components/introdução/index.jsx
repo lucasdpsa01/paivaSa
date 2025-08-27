f5c6e647d2abe709d1eb0923545430e18ffa8022
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-08-27 18:00:39 -0300

    mudei o nome

--- sla/src/components/introdução/index.jsx
+++ sla/src/components/introdução/index.jsx
@@ -6,7 +6,7 @@ export default function Introducao(){
     return(
         <div className="introducao" id="introducao">
             <div className="introducao-text">
-                <h1>Olá, eu sou Paiva Sá</h1>
+                <h1>Olá, eu sou Paiva de Sá</h1>
                 <p>Estou sempre aprimorado minhas skills em React and Node para ser um programador full-stack. Meu objetivo é criar ferramentas que ajudem as pessoas e sempre buscando conhecimento.</p>
             </div>
             <img src={minhafoto} alt="minha foto" height={250} className="introducao-imagem"/>
