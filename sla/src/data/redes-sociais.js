b46392105334a52806489f006ff2d6bbd98ca5b4
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-09-11 22:42:07 -0300

    .

--- sla/src/data/redes-sociais.js
+++ sla/src/data/redes-sociais.js
@@ -4,6 +4,6 @@ import linkedin from "../assets/linkedin.png"
 
 export const redes_sociais = [
     {id: 1, nome: "github", images: github, url: "https://github.com/lucasdpsa01"},
-    {id: 2, nome: "linkedin", images: linkedin, url: "www.linkedin.com/in/lucas-daniel-paiva-de-sá-978498290"},
+    {id: 2, nome: "linkedin", images: linkedin, url: "www.linkedin.com/in/paiva-sa01"},
     {id: 3, nome: "kaggle", images: kaggle, url: "https://www.kaggle.com/lucass01"}
 ]
