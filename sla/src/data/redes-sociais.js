d08f43bbe015a2dc67baa6f5807558325a928174
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-09-11 22:13:18 -0300

    .

--- sla/src/data/redes-sociais.js
+++ sla/src/data/redes-sociais.js
@@ -1,5 +1,3 @@
-import insta from "../assets/instagram.png"
-import facebook from "../assets/facebook.png"
 import github from "../assets/github.png"
 import kaggle from "../assets/kaggle.png"
 import linkedin from "../assets/linkedin.png"
@@ -7,7 +5,5 @@ import linkedin from "../assets/linkedin.png"
 export const redes_sociais = [
     {id: 1, nome: "github", images: github, url: "https://github.com/lucasdpsa01"},
     {id: 2, nome: "linkedin", images: linkedin, url: "www.linkedin.com/in/lucas-daniel-paiva-de-sá-978498290"},
-    {id: 3, nome: "kaggle", images: kaggle, url: "https://www.kaggle.com/lucass01"},
-    {id: 4, nome: "instagram", images: insta, url: "https://www.instagram.com/lucas.paiva01"},
-    {id: 5, nome: "facebook", images: facebook, url: "https://www.facebook.com/oimiga.aliko.3"}
+    {id: 3, nome: "kaggle", images: kaggle, url: "https://www.kaggle.com/lucass01"}
 ]
