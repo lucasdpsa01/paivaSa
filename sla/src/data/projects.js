87b8c0ecee5653987de63df5601bdd03ca4d08ee
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-09-11 22:15:02 -0300

    .

--- sla/src/data/projects.js
+++ sla/src/data/projects.js
@@ -1,5 +1,5 @@
 export const projects = [
-    {id: 1, nome: "PaivaCritic", descricao: "Aplicação WEB full stack de avaliação de jogos e envio de sugestões.", site:"https://github.com/lucasdpsa01/front---paivacritic.git"},
+    {id: 1, nome: "PaivaCritic", descricao: "Aplicação WEB full stack de avaliação de jogos e envio de sugestões.", site:"https://github.com/lucasdpsa01/paivacritic.git"},
     {id: 2, nome: "Análise bancária", descricao: "Coleta, limpeza de dados e criação de gráficos sobre o dataset bancário.", site:"https://www.kaggle.com/code/lucass01/escolaridade-dos-clientes"},
     {id: 3, nome: "Análise Lol", descricao: "Coleta dos datasets dos campeôes do jogo League of Legends e mostrando graficamente sobre eles em geral.", site:"https://www.kaggle.com/code/lucass01/distribui-o-de-campe-es-melee-e-ranged-em-league"},
     {id: 4, nome: "Plataforma de ensino", descricao: "Aplicação web de ensino virtual, onde monitores/professores possam mandar suas aulas, tarefas e certificados e os alunos acessem, baixem e enviem suas tarefas e dúvidas.", site:"https://github.com/FelipeTolentino21/projeto-integrador.git"}
