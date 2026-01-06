0ea052048dd042556945716d3b46add5265f94a4
lucasdpsa01 <lucasdsa2801@gmail.com>
2026-01-06 10:44:30 -0300

    modo claro e escuro logicamente feito

--- sla/src/components/header/index.jsx
+++ sla/src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
 import "./header.css"
 
 export default function Header() {
@@ -10,15 +10,32 @@ export default function Header() {
             window.scrollTo({ top, behavior: 'smooth' });
         }
     };
+
+    const [darkMode, setDarkMode] = useState(false);
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add("dark");
+            document.body.classList.remove("light");
+        } else {
+            document.body.classList.add("light");
+            document.body.classList.remove("dark");
+        }
+    })
     
     return (
         <div className="header">
             <h1>Portfólio</h1>
             <div className="header-links">
-                <button onClick={() => scrollToSection('Experiences')}>Experiências</button>
                 <button onClick={() => scrollToSection('Skills')}>Skills</button>
+                <button onClick={() => scrollToSection('Experiences')}>Experiências</button>
                 <button onClick={() => scrollToSection('Projetos')}>Projetos</button>
                 <button onClick={() => scrollToSection('Contact')}>Contato</button>
+                <img src={darkMode ? "https://unpwemjugtntyiedmdvc.supabase.co/storage/v1/object/public/images/moon.png" : "https://unpwemjugtntyiedmdvc.supabase.co/storage/v1/object/public/images/sun.png"} 
+                alt="Alternar tema"
+                height={35}
+                onClick={() => setDarkMode(!darkMode)}
+                className="header-img"
+                />
             </div>
         </div>
     )
--- sla/src/components/introdução/index.jsx
+++ sla/src/components/introdução/index.jsx
@@ -7,7 +7,7 @@ export default function Introducao() {
     return (
         <div className="introducao" id="introducao">
             <div className="introducao-text">
-                <h1>Olá, eu sou Paiva de Sá</h1>
+                <h2>Olá, eu sou Paiva de Sá</h2>
                 <p>Estou sempre aprimorado minhas skills em React and Node para ser um programador full-stack. Meu objetivo é criar ferramentas que ajudem as pessoas e sempre buscando conhecimento.</p>
                 <div className="links-icon">
                     {redes_sociais.map((redes) => (
